815d71e8b59f493f41f7546be486a338619f6c10
DevanFarmer <devanf21@gmail.com>
2025-04-29 22:19:16 +0200

    Created string formatting for description
    Allows you to make "dynamic" descriptions that show the value of stat modifiers.

--- Assets/Scripts/Perk/Perk.cs
+++ Assets/Scripts/Perk/Perk.cs
@@ -1,15 +1,20 @@
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 
 public abstract class Perk : ScriptableObject
 {
     [Header("General")]
     public string perkName;
+    [TextArea(1, 10)]
     public string description;
+    [TextArea(1, 10)]
+    public string formattedDescription;
+    public string effectDescription;
 
     [Header("Passive Info")]
     public List<StatModifier> statModifiers = new List<StatModifier>();
-    public bool hasActiveLogic;
+    protected bool hasActiveLogic = true;
 
     // Pass owner for any logic that might need it
     public virtual void OnEquip(GameObject owner)
@@ -20,7 +25,10 @@ public abstract class Perk : ScriptableObject
         }
     }
 
-    public abstract void OnUpdate(GameObject owner);
+    public virtual void OnUpdate(GameObject owner)
+    {
+        // Could make abstract but for few cases might not have update needed
+    }
 
     public virtual void OnUnEquip(GameObject owner)
     {
@@ -33,4 +41,49 @@ public abstract class Perk : ScriptableObject
     }
 
     // Add self as a listener for events like enemy death, exp gained, hit, healed etc.
+
+    public bool HasActiveLogic()
+    {
+        return hasActiveLogic;
+    }
+
+#if UNITY_EDITOR
+    void OnValidate()
+    {
+        UpdateEffectDescriptionFromStats();
+        FormatDescription();
+    }
+
+    public void UpdateEffectDescriptionFromStats()
+    {
+        effectDescription = "";
+        foreach (StatModifier mod in statModifiers)
+        {
+            string color = mod.value >= 0 ? "green" : "red";
+            string sign = mod.value >= 0 ? "+" : "-";
+            if (effectDescription != "") effectDescription += "\n"; // Only break line when adding a stat, ignores first
+            effectDescription += $"<color={color}>{sign}{Mathf.Abs(mod.value)}</color>"; // Adds value, adds color
+            if (mod.bonusType == PassiveValueType.Percentage) effectDescription += "%"; // shows if percentage
+            effectDescription += $" {mod.targetStat}"; // Adds stat type
+        }
+    }
+
+    public void FormatDescription()
+    {
+        // Could add the color by making the list be of string and check value
+        List<float> values = new();
+        foreach (StatModifier statModifier in statModifiers)
+        {
+            values.Add(statModifier.value);
+        }
+        formattedDescription = string.Format(description, values.Cast<object>().ToArray());
+
+        // Example:
+        //List<int> statValues = new List<int> { 15, 2 };
+        //string format = "Health: {0}, Attack: {1}";
+
+        //string result = string.Format(format, statValues.Cast<object>().ToArray());
+        //Output: "Health: 15, Attack: 2"
+    }
+#endif
 }
