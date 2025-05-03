c26652988b5e194e7607891b5b90aae2c30d0765
DevanFarmer <devanf21@gmail.com>
2025-05-03 17:39:54 +0200

    Added a constructor

--- Assets/Scripts/Data Classes/StatModifier.cs
+++ Assets/Scripts/Data Classes/StatModifier.cs
@@ -4,4 +4,11 @@ public class StatModifier
     public StatType targetStat;
     public float value;
     public PassiveValueType bonusType;
+
+    public StatModifier(StatType targetStat, float value, PassiveValueType bonusType)
+    {
+        this.targetStat = targetStat;
+        this.value = value;
+        this.bonusType = bonusType;
+    }
 }
