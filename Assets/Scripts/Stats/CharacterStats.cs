9270d518ea79a5ff9f805075e3cfe857d52ba20f
DevanFarmer <devanf21@gmail.com>
2025-05-04 21:02:32 +0200

    Created base Character Stats scriptable object

---
+++ Assets/Scripts/Stats/CharacterStats.cs
@@ -0,0 +1,13 @@
+using UnityEngine;
+
+public class CharacterStats : ScriptableObject
+{
+    public float Health;
+    public float AttackPower;
+    public float AttackSpeed;
+
+    public int InvincibilityHits;
+
+    // public float MinionAttackSpeed; // have things like player, enemy and minion stats for non general stats
+    // Add as needed
+}
