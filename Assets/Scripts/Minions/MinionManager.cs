e9ebde7db150d964c54790583025a808b02ee8bb
DevanFarmer <devanf21@gmail.com>
2025-05-12 15:54:41 +0200

    Created minion manager script

---
+++ Assets/Scripts/Minions/MinionManager.cs
@@ -0,0 +1,40 @@
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MinionManager : MonoBehaviour
+{
+    [SerializeField] List<MinionSpawnData> minions = new();
+
+    private void Update()
+    {
+        HandleInput();
+    }
+
+    // will probably use new input system but for now use legacy
+    void HandleInput()
+    {
+        if (Input.GetKeyDown(KeyCode.Q))
+        {
+            SpawnMinion(minions[0]);
+        }
+        else if (Input.GetKeyUp(KeyCode.W))
+        {
+            SpawnMinion(minions[1]);
+        }
+        else if (Input.GetKeyUp(KeyCode.E))
+        {
+            SpawnMinion(minions[2]);
+        }
+        else if (Input.GetKeyUp(KeyCode.R))
+        {
+            SpawnMinion(minions[3]);
+        }
+    }
+
+    void SpawnMinion(MinionSpawnData spawnData)
+    {
+        // get mouse position
+        // spawn prefab
+        // set base stats
+    }
+}
