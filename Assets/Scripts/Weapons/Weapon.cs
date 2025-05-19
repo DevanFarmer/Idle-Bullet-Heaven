530e03d35b9eb5e7300c09fcd4b82fc7bbb23ad3
DevanFarmer <devanf21@gmail.com>
2025-05-19 17:48:38 +0200

    Removed weapon as a child of perk

--- Assets/Scripts/Weapons/Weapon.cs
+++ Assets/Scripts/Weapons/Weapon.cs
@@ -1,19 +1,40 @@
 using UnityEngine;
 
-public abstract class Weapon : Perk
+public abstract class Weapon : ScriptableObject
 {
+    [Header("General")]
+    public string weaponName;
+    [TextArea(1, 10)]
+    public string description;
+
+    [Header("Set up")]
     public GameObject weaponPrefab;
 
     public float power;
-    public float attackSpeed; // Cooldown
-    public float range;
+    public float attackSpeed; // "Cooldown"
+    public float range; // is this needed for all weapons? Each has something similar but not the same name
 
     protected float lastAttackTime;
 
-    // abstract Attack method?
-    // can then override OnUpdate with:
-    //if (lastAttackTime + attackSpeed <= Time.time)
-    //{
-            //Attack();
-    //}
+    protected StatManager statManager;
+
+    public virtual void OnObtained(GameObject owner)
+    {
+        statManager = owner.GetComponent<StatManager>();
+        lastAttackTime = Time.time;
+    }
+
+    public virtual void OnUpdate(GameObject owner)
+    {
+        if (lastAttackTime + attackSpeed <= Time.time)
+        {
+            Attack();
+            lastAttackTime = Time.time;
+        }
+    }
+
+    protected virtual void Attack()
+    {
+
+    }
 }
