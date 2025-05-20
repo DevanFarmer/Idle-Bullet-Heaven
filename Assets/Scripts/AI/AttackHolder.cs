ddfaeb913dbb17198d134c06d6b3142a73cef43f
DevanFarmer <devanf21@gmail.com>
2025-05-20 13:01:31 +0200

    Created attack holder

---
+++ Assets/Scripts/AI/AttackHolder.cs
@@ -0,0 +1,50 @@
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class AttackHolder : MonoBehaviour
+{
+    [SerializeField] BaseAttack attack; // list be a list, but need to think how to handle each timer
+    [SerializeField] LayerMask targetMask;
+
+    public UnityEvent<bool> onInRangeChanged;
+
+    float lastAttackTime;
+
+    bool inRange;
+
+    void Start()
+    {
+        attack.Equip(transform, targetMask);
+        lastAttackTime = Time.time;
+
+        inRange = false;
+    }
+
+    void Update()
+    {
+        CheckInRange();
+
+        if (inRange && lastAttackTime + attack.speed <= Time.time)
+        {
+            attack.Attack();
+            lastAttackTime = Time.time;
+        }
+    }
+
+    void CheckInRange()
+    {
+        bool rangeState = attack.InRange();
+
+        if (rangeState != inRange)
+        {
+            inRange = rangeState;
+            onInRangeChanged?.Invoke(inRange);
+        }
+    }
+
+    public void TestMethod()
+    {
+        Debug.Log($"Ping! From {this.name} on {gameObject.name}");
+    }
+}
