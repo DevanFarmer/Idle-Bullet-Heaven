7e6fc89334cc4860e71243fc3723e29e5df36e67
DevanFarmer <devanf21@gmail.com>
2025-05-28 23:28:26 +0200

    Created empty PerkSelector script

---
+++ Assets/Scripts/Perk/Selecting Perks/PerkSelector.cs
@@ -0,0 +1,16 @@
+using System.Collections.Generic;
+using UnityEngine;
+
+// handles validating selectable perks and choosing for selection
+public class PerkSelector : MonoBehaviour
+{
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+}
