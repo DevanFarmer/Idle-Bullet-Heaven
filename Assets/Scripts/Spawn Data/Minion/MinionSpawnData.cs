364e6419569f2ce29551b2915805ad72bcc65ff4
DevanFarmer <devanf21@gmail.com>
2025-06-05 22:00:17 +0200

    Modified to be Character Spawn Data

--- Assets/Scripts/Spawn Data/Minion/MinionSpawnData.cs
+++ Assets/Scripts/Spawn Data/Minion/MinionSpawnData.cs
@@ -1,13 +1,43 @@
-using System.Collections.Generic;
+using EventBusEventData;
 using UnityEngine;
 
 [CreateAssetMenu(fileName = "New MinionSpawnData", menuName = "Minion/Spawn Data")]
-public class MinionSpawnData : ScriptableObject // make a base spawn data so with name, prefab etc and us as when handling in spawning managers
+public class MinionSpawnData : CharacterSpawnData // make a base spawn data so with name, prefab etc and us as when handling in spawning managers
 {
-    public GameObject minionPrefab;
-    public CharacterStats stats; // will store minionstats
     public float spawnCooldown;
-    public List<Perk> perks = new(); // not good idea
 
-    // could set base stats here instead of manager, but having them in one place feels better
+    public override GameObject Spawn(Vector3 spawnPos)
+    {
+        GameObject minion = base.Spawn(spawnPos);
+
+        IHealth health = minion.GetComponent<IHealth>();
+
+        AddOnHitActions(minion, health);
+        AddOnDeathActions(minion, health);
+
+        return minion;
+    }
+
+    protected override void AddOnHitActions(GameObject spawnObject, IHealth health)
+    {
+        health.AddOnHitAction(SetCharacterOnHitEvent);
+    }
+
+    protected override void AddOnDeathActions(GameObject spawnObject, IHealth health)
+    {
+        health.AddOnDeathAction(() => EventBus.Publish(new MinionDeathEvent()));
+        health.AddOnDeathAction(() => Destroy(spawnObject));
+    }
+
+    protected override void SetCharacterOnHitEvent(float damage, Transform spawnObject)
+    {
+        EventBus.Publish(new MinionHitEvent(damage));
+    }
+
+    protected override IHealth SetHealthComponent(GameObject spawnObject)
+    {
+        IHealth health = base.SetHealthComponent(spawnObject);
+        health.UpdateMaxHealth();
+        return health;
+    }
 }
