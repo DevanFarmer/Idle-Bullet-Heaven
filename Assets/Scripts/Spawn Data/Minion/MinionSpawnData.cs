79291d8aa7cb3d6cc542c36a50ff829f23459f3a
DevanFarmer <devanf21@gmail.com>
2025-06-10 17:06:56 +0200

    Added incomplete weapon setting up

--- Assets/Scripts/Spawn Data/Minion/MinionSpawnData.cs
+++ Assets/Scripts/Spawn Data/Minion/MinionSpawnData.cs
@@ -1,10 +1,12 @@
 using EventBusEventData;
+using System.Collections.Generic;
 using UnityEngine;
 
 [CreateAssetMenu(fileName = "New MinionSpawnData", menuName = "Spawn Data/Minion")]
 public class MinionSpawnData : NPCSpawnData // make a base spawn data so with name, prefab etc and us as when handling in spawning managers
 {
     public float baseSpawnCooldown; // should rather be a stat honestly, since can be modified, made into base cooldown instead
+    public List<Weapon> baseWeapons = new(); // i like the idea of minions being able to get weapons like the player, can just be ignored if not good idea
 
     public override GameObject Spawn(Vector3 spawnPos)
     {
@@ -39,6 +41,15 @@ public class MinionSpawnData : NPCSpawnData // make a base spawn data so with na
         IHealth health = base.SetHealthComponent(spawnObject);
         health.UpdateMaxHealth();
         return health;
+    }
+
+    void SetWeapons(GameObject minion)
+    {
+        WeaponManager weaponManager = minion.GetComponent<WeaponManager>();
+        if (weaponManager == null) { /* ignore sending warning since might not use this */ weaponManager = minion.AddComponent<WeaponManager>(); }
+
+        // problem with obtaining on start since if i now add them here they will be added twice
+        // ignore for now
     }
 
     // + decay manager - nevermind I forgot manager already handles that
